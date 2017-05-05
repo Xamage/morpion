3c6b26608bbf78d6e9894d6fe1ba667557dfc669
Xavier Masclet <xavier.masclet@axa.fr>
2017-05-05 12:53:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+class Morpion extends React.Component {
+    render() {
+        return (
+            <div class="case">une case</div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Morpion />,
+    document.getElementById("root")
+);
